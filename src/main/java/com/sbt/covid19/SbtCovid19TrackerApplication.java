d01cdf87a17d09c74d8afb05c999e58b91352bf5
Arockiaraj Savarinathan <arockiarajs24@gmail.com>
2020-02-28 20:17:53 +0800

    initial upload

---
+++ src/main/java/com/sbt/covid19/SbtCovid19TrackerApplication.java
@@ -0,0 +1,13 @@
+package com.sbt.covid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbtCovid19TrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbtCovid19TrackerApplication.class, args);
+	}
+
+}
